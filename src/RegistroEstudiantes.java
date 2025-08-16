e918ed4b4609b6d61fc9024714ad9ffb88a8771f
Cmunoz <cmunozm6@miumg.edu.gt>
2025-08-16 10:21:27 -0600

    RegistroEstudaintes

---
+++ src/RegistroEstudiantes.java
@@ -0,0 +1,38 @@
+public class RegistroEstudiantes {
+    private int Carné;
+    private String NombreEstudinte;
+    private String ApellidoEstudinte;
+    private int FechaNacimiento;
+
+    public int getCarné() {
+        return Carné;
+    }
+
+    public void setCarné(int carné) {
+        Carné = carné;
+    }
+
+    public String getNombreEstudinte() {
+        return NombreEstudinte;
+    }
+
+    public void setNombreEstudinte(String nombreEstudinte) {
+        NombreEstudinte = nombreEstudinte;
+    }
+
+    public String getApellidoEstudinte() {
+        return ApellidoEstudinte;
+    }
+
+    public void setApellidoEstudinte(String apellidoEstudinte) {
+        ApellidoEstudinte = apellidoEstudinte;
+    }
+
+    public int getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(int fechaNacimiento) {
+        FechaNacimiento = fechaNacimiento;
+    }
+}
